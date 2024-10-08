6d8938ecd4dd240be4309841110002b3f61a7f00
NihalSagar <130696098+NihalSagar@users.noreply.github.com>
2024-10-08 16:04:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const sequelize = require('./config/database');
-// const productRoutes = require('./routes/productRoutes');
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// // Use product routes
-// app.use('/api/products', productRoutes);
-
-// // Sync the database and start the server
-// const startServer = async () => {
-//     try {
-//         await sequelize.sync(); // Sync the models with the database
-//         console.log('Database synchronized successfully.');
-//         app.listen(3000, () => {
-//             console.log('Server is running on port 3000');
-//         });
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//     }
-// };
-
-// startServer();
-
-
 const express = require('express');
 const sequelize = require('./config/database');
 const productRoutes = require('./routes/productRoutes');
